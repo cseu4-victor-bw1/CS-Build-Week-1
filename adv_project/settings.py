4fd3f5d8e94f1296604ec915c55771b891a6a348
dueka <ekanemd@live.com>
2020-04-27 10:56:32 +0100

    heroku push

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -93,16 +93,20 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'lambda_mud_db',
-        'USER': 'process.env.user',
-        'PASSWORD': "process.env.password",
-        'HOST': 'localhost',
-        'PORT': '5432',
-    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'lambda_mud_db',
+    #     'USER': 'process.env.user',
+    #     'PASSWORD': "process.env.password",
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # },
     # To discuss with victor
-    dj_database_url.parse('postgres://...', conn_max_age=600)
+    # dj_database_url.parse('postgres://...', conn_max_age=600)
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # Password validation
