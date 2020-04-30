5a50de290b257b5bfbd7d58a979618a9c37879a1
dueka <ekanemd@live.com>
2020-04-30 11:29:06 +0100

    added registration and auth

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    # path('', include('rest_auth.urls')),
-    # path('registration/', include('rest_auth.registration.urls')),
+    path('', include('rest_auth.urls')),
+    path('registration/', include('rest_auth.registration.urls')),
     path('welcome', views.welcome)
 ]
